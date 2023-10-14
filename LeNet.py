e74632f928d9811d6f3340076739116d0cd07dfc
guagua <1677574244@qq.com>
2023-10-14 13:07:53 +0800

    GPU

--- LeNet.py
+++ LeNet.py
@@ -9,6 +9,7 @@ from d2l import torch as d2l
 class Reshape(torch.nn.Module):
     def forward(self, x):
         return x.view(-1, 1, 28, 28)
+    
 net = torch.nn.Sequential(
     Reshape(),
     nn.Conv2d(1, 6, kernel_size=5, padding=2),nn.Sigmoid(),
@@ -21,4 +22,23 @@ net = torch.nn.Sequential(
     nn.Linear(84, 10)
 )
 
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+# GPU计算
+def evaluate_acc_gpu(net, data_iter, device=None):
+    if isinstance(net, nn.Module):
+        net.eval() # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    metric = d2l.Accumulator(2) #累加器，正确预测的数量 总预测的数量
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                X = [x.to(device)for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(d2l.accuracy(net(X), y), y.numel()) 
+    return metric[0] / metric[1]
 
